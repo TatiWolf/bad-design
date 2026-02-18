be1ad4d646f6e87fd1f01e1f665f4204d91eacd4
Tatiana Wolf <114405573+TatiWolf@users.noreply.github.com>
2026-02-18 12:41:51 +0300

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './js/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    clean: true,
+    filename: './js/app.js',
+  },
+};
