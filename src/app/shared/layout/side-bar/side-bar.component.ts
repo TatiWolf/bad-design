1c77d4f440b61aa1b7724275e8bab79b8f7d8e0e
TatiWolf <tat.wolf.work@gmail.com>
2026-03-04 20:51:27 +0300

    updated sidebar

--- src/app/shared/layout/side-bar/side-bar.component.ts
+++ src/app/shared/layout/side-bar/side-bar.component.ts
@@ -1,7 +1,10 @@
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {ActivatedRoute, Router, RouterLink, RouterLinkActive} from '@angular/router';
 import {DarkPatternService} from '../../../features/dark-pattern/services/dark-pattern.service';
 import {NgOptimizedImage} from '@angular/common';
+import {map} from 'rxjs';
+import {isCatalogType} from '../../../features/dark-pattern/pages/models/catalog.type';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 
 @Component({
@@ -18,11 +21,42 @@ import {NgOptimizedImage} from '@angular/common';
 })
 export class SideBarComponent {
   private darkPatternService = inject(DarkPatternService);
-
+  private router: Router = inject(Router);
   readonly catalogs = this.darkPatternService.catalogs
+  private route = inject(ActivatedRoute);
 
   closeTest(event: MouseEvent) {
-    //   event.stopPropagation();
-    // this.router.navigate(['/']);
+    event.stopPropagation();
+    event.preventDefault();
+    const id = this.route.firstChild?.snapshot.paramMap.get('id');
+    const type = this.route.firstChild?.snapshot.paramMap.get('type');
+      if (Number(id)) {
+        this.router.navigate(['/', type]);
+
+        } else {
+        this.router.navigate(['/']);
+
+      }
+
   }
+
+  readonly id = toSignal(
+    this.route.url.pipe(
+      map(() => {
+        let route = this.route;
+        while (route.firstChild) route = route.firstChild;
+        const id = route.snapshot.paramMap.get('id');
+        return id ? Number(id) : null;
+      })
+    ),
+    { initialValue: null }
+  );
+
+  readonly type = toSignal(
+    this.route.paramMap.pipe(
+      map(params => params.get('type')),
+      map(raw => isCatalogType(raw) ? raw : null)
+    ),
+    {initialValue: null}
+  );
 }
