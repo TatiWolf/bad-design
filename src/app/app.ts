19ca516f1113f66b18da3f7ab68f614af1fe6a2b
TatiWolf <tat.wolf.work@gmail.com>
2026-03-04 21:42:46 +0300

    added animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
+@ViewChild('intro')intro!: ElementRef
+
+  ngAfterViewInit() {
+
+    if (this.checkLocalStorage()) {
+      this.intro.nativeElement.remove();
+      return;
+    }
+
+    setTimeout(() => {
+      this.intro.nativeElement.classList.add('intro-hide');
+    }, 2500);
+
+    setTimeout(() => {
+      this.intro.nativeElement.remove();
+      localStorage.setItem('animation', '1');
+    }, 3500);
+}
+
+  checkLocalStorage() {
+  return !!localStorage.getItem('animation')
+  }
+
 }
