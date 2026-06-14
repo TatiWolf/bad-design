6183acf49f8c13bfdfcf07c1b7d2524140ad73f2
TatiWolf <tat.wolf.work@gmail.com>
2026-06-14 18:05:16 +0300

    check bag with width screen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,15 @@ import {RouterOutlet} from '@angular/router';
 })
 export class App implements AfterViewInit {
 @ViewChild('intro')intro!: ElementRef
-
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      document
+        .querySelector('.main__header__router')
+        ?.scrollTo(0, 0);
+    });
+  }
   ngAfterViewInit() {
 
     if (this.checkLocalStorage()) {
